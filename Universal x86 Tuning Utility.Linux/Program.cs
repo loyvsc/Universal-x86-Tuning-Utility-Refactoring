0bef2f7223fbba16e9d7b409c6982228ed168a7e
Valery Varaksa <varaksav62@gmail.com>
2025-07-20 22:40:59 +0300

    Fix Linux & Windows app configuring

--- Universal x86 Tuning Utility.Linux/Program.cs
+++ Universal x86 Tuning Utility.Linux/Program.cs
@@ -8,35 +8,6 @@ using Universal_x86_Tuning_Utility.Linux.Services.GPUs;
 
 namespace Universal_x86_Tuning_Utility.Linux;
 
-class LinuxApplication : App
-{
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SplatRegistrations.RegisterLazySingleton<IASUSWmiService, LinuxAsusWmiService>();
-        SplatRegistrations.RegisterLazySingleton<ICliService, LinuxCliService>();
-        SplatRegistrations.RegisterLazySingleton<ICpuControlService, LinuxCpuControlService>();
-        SplatRegistrations.RegisterLazySingleton<IDisplayInfoService, LinuxDisplayInfoService>();
-        SplatRegistrations.RegisterLazySingleton<IFanControlService, LinuxFanControlService>();
-        SplatRegistrations.RegisterLazySingleton<IGameLauncherService, LinuxGameLauncherService>();
-        SplatRegistrations.RegisterLazySingleton<IAmdGpuService, LinuxAmdGpuService>();
-        SplatRegistrations.RegisterLazySingleton<INvidiaGpuService, LinuxNvidiaGpuService>();
-        SplatRegistrations.RegisterLazySingleton<IIntelManagementService, LinuxIntelManagementService>();
-        SplatRegistrations.RegisterLazySingleton<IPowerPlanService, LinuxPowerPlanService>();
-        SplatRegistrations.RegisterLazySingleton<ISensorsService, LinuxSensorsService>();
-        SplatRegistrations.RegisterLazySingleton<IRtssService, LinuxRtssService>();
-        SplatRegistrations.RegisterLazySingleton<IStressTestService, LinuxStressTestService>();
-        SplatRegistrations.RegisterLazySingleton<ISystemBootService, LinuxSystemBootService>();
-        SplatRegistrations.RegisterLazySingleton<ISystemInfoService, LinuxSystemInfoService>();
-        SplatRegistrations.RegisterLazySingleton<IUpdateService, UpdateService>();
-        SplatRegistrations.RegisterLazySingleton<IUpdateInstallerService, LinuxUpdateInstallerService>();
-        SplatRegistrations.RegisterLazySingleton<IBatteryInfoService, LinuxBatteryInfoService>();
-        
-        SplatRegistrations.SetupIOC();
-    }
-}
-
 class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
@@ -48,8 +19,31 @@ class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<LinuxApplication>()
+        => AppBuilder.Configure<App>()
             .UseX11()
             .UseSkia()
+            .AfterSetup(_ =>
+            {
+                SplatRegistrations.RegisterLazySingleton<IASUSWmiService, LinuxAsusWmiService>();
+                SplatRegistrations.RegisterLazySingleton<ICliService, LinuxCliService>();
+                SplatRegistrations.RegisterLazySingleton<ICpuControlService, LinuxCpuControlService>();
+                SplatRegistrations.RegisterLazySingleton<IDisplayInfoService, LinuxDisplayInfoService>();
+                SplatRegistrations.RegisterLazySingleton<IFanControlService, LinuxFanControlService>();
+                SplatRegistrations.RegisterLazySingleton<IGameLauncherService, LinuxGameLauncherService>();
+                SplatRegistrations.RegisterLazySingleton<IAmdGpuService, LinuxAmdGpuService>();
+                SplatRegistrations.RegisterLazySingleton<INvidiaGpuService, LinuxNvidiaGpuService>();
+                SplatRegistrations.RegisterLazySingleton<IIntelManagementService, LinuxIntelManagementService>();
+                SplatRegistrations.RegisterLazySingleton<IPowerPlanService, LinuxPowerPlanService>();
+                SplatRegistrations.RegisterLazySingleton<ISensorsService, LinuxSensorsService>();
+                SplatRegistrations.RegisterLazySingleton<IRtssService, LinuxRtssService>();
+                SplatRegistrations.RegisterLazySingleton<IStressTestService, LinuxStressTestService>();
+                SplatRegistrations.RegisterLazySingleton<ISystemBootService, LinuxSystemBootService>();
+                SplatRegistrations.RegisterLazySingleton<ISystemInfoService, LinuxSystemInfoService>();
+                SplatRegistrations.RegisterLazySingleton<IUpdateService, UpdateService>();
+                SplatRegistrations.RegisterLazySingleton<IUpdateInstallerService, LinuxUpdateInstallerService>();
+                SplatRegistrations.RegisterLazySingleton<IBatteryInfoService, LinuxBatteryInfoService>();
+        
+                SplatRegistrations.SetupIOC();
+            })
             .LogToTrace();
 }
--- Universal x86 Tuning Utility.Windows/Program.cs
+++ Universal x86 Tuning Utility.Windows/Program.cs
@@ -9,35 +9,6 @@ using Universal_x86_Tuning_Utility.Windows.Services.SystemInfoServices;
 
 namespace Universal_x86_Tuning_Utility.Windows;
 
-public partial class WindowsApp : App
-{
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SplatRegistrations.RegisterLazySingleton<IASUSWmiService, WindowsAsusWmiService>();
-        SplatRegistrations.RegisterLazySingleton<ICliService, WindowsCliService>();
-        SplatRegistrations.RegisterLazySingleton<ICpuControlService, WindowsCpuControlService>();
-        SplatRegistrations.RegisterLazySingleton<IDisplayInfoService, WindowsDisplayInfoService>();
-        SplatRegistrations.RegisterLazySingleton<IFanControlService, WindowsFanControlService>();
-        SplatRegistrations.RegisterLazySingleton<IGameLauncherService, WindowsGameLauncherService>();
-        SplatRegistrations.RegisterLazySingleton<IAmdGpuService, WindowsAmdGpuService>();
-        SplatRegistrations.RegisterLazySingleton<INvidiaGpuService, WindowsNvidiaGpuService>();
-        SplatRegistrations.RegisterLazySingleton<IIntelManagementService, WindowsIntelManagementService>();
-        SplatRegistrations.RegisterLazySingleton<IPowerPlanService, WindowsPowerPlanService>();
-        SplatRegistrations.RegisterLazySingleton<IRyzenAdjService, RyzenAdjService>();
-        SplatRegistrations.RegisterLazySingleton<ISensorsService, WindowsSensorsService>();
-        SplatRegistrations.RegisterLazySingleton<IRtssService, WindowsRtssService>();
-        SplatRegistrations.RegisterLazySingleton<IStressTestService, WindowsStressTestService>();
-        SplatRegistrations.RegisterLazySingleton<ISystemBootService, WindowsSystemBootService>();
-        SplatRegistrations.RegisterLazySingleton<ISystemInfoService, WindowsSystemInfoService>();
-        SplatRegistrations.RegisterLazySingleton<IUpdateInstallerService, WindowsUpdateInstallerService>();
-        SplatRegistrations.RegisterLazySingleton<IBatteryInfoService, WindowsBatteryInfoService>();
-        
-        SplatRegistrations.SetupIOC();
-    }
-}
-
 class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
@@ -49,8 +20,31 @@ class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<WindowsApp>()
+        => AppBuilder.Configure<App>()
             .UseWin32()
             .WithInterFont()
+            .AfterSetup(_ =>
+            {
+                SplatRegistrations.RegisterLazySingleton<IASUSWmiService, WindowsAsusWmiService>(); 
+                SplatRegistrations.RegisterLazySingleton<ICliService, WindowsCliService>();
+                SplatRegistrations.RegisterLazySingleton<ICpuControlService, WindowsCpuControlService>();
+                SplatRegistrations.RegisterLazySingleton<IDisplayInfoService, WindowsDisplayInfoService>();
+                SplatRegistrations.RegisterLazySingleton<IFanControlService, WindowsFanControlService>();
+                SplatRegistrations.RegisterLazySingleton<IGameLauncherService, WindowsGameLauncherService>();
+                SplatRegistrations.RegisterLazySingleton<IAmdGpuService, WindowsAmdGpuService>();
+                SplatRegistrations.RegisterLazySingleton<INvidiaGpuService, WindowsNvidiaGpuService>();
+                SplatRegistrations.RegisterLazySingleton<IIntelManagementService, WindowsIntelManagementService>();
+                SplatRegistrations.RegisterLazySingleton<IPowerPlanService, WindowsPowerPlanService>();
+                SplatRegistrations.RegisterLazySingleton<IRyzenAdjService, RyzenAdjService>();
+                SplatRegistrations.RegisterLazySingleton<ISensorsService, WindowsSensorsService>();
+                SplatRegistrations.RegisterLazySingleton<IRtssService, WindowsRtssService>();
+                SplatRegistrations.RegisterLazySingleton<IStressTestService, WindowsStressTestService>();
+                SplatRegistrations.RegisterLazySingleton<ISystemBootService, WindowsSystemBootService>();
+                SplatRegistrations.RegisterLazySingleton<ISystemInfoService, WindowsSystemInfoService>();
+                SplatRegistrations.RegisterLazySingleton<IUpdateInstallerService, WindowsUpdateInstallerService>();
+                SplatRegistrations.RegisterLazySingleton<IBatteryInfoService, WindowsBatteryInfoService>();
+        
+                SplatRegistrations.SetupIOC();
+            })
             .LogToTrace();
 }
