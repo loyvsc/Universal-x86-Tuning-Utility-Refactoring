6fd6257976d8285facd55eda6a6ef404dc392724
Valery Varaksa <varaksav62@gmail.com>
2025-07-22 12:02:50 +0300

    Update WindowsSystemBootService

--- Universal x86 Tuning Utility.Windows/Services/WindowsSystemBootService.cs
+++ Universal x86 Tuning Utility.Windows/Services/WindowsSystemBootService.cs
@@ -9,7 +9,8 @@ public class WindowsSystemBootService : ISystemBootService
     public void CreateTask(string taskName, string pathToExecutable, string arguments = "", string taskDescription = "")
     {
         var taskService = TaskService.Instance;
-        if (taskService.RootFolder.AllTasks.All(t => t.Name != taskName))
+        var task = taskService.RootFolder.AllTasks.FirstOrDefault(t => t.Name != taskName);
+        if (task == null)
         {
             // Create a new task definition and assign properties
             var taskDefinition = taskService.NewTask();
@@ -22,6 +23,24 @@ public class WindowsSystemBootService : ISystemBootService
             // Create a trigger that will fire the task at this time every other day
             taskDefinition.Triggers.Add(new LogonTrigger());
             
+            taskDefinition.Actions.Add(pathToExecutable);
+
+            taskService.RootFolder.RegisterTaskDefinition(taskName, taskDefinition);
+        }
+        else
+        {
+            // Update task definition properties
+            var taskDefinition = task.Definition;
+            taskDefinition.Principal.RunLevel = TaskRunLevel.Highest;
+            taskDefinition.RegistrationInfo.Description = taskDescription;
+            taskDefinition.Settings.DisallowStartIfOnBatteries = false;
+            taskDefinition.Settings.StopIfGoingOnBatteries = false;
+            taskDefinition.Settings.DisallowStartOnRemoteAppSession = false;
+            
+            taskDefinition.Triggers.Clear();
+            taskDefinition.Triggers.Add(new LogonTrigger());
+            
+            taskDefinition.Actions.Clear();
             taskDefinition.Actions.Add(pathToExecutable);
 
             taskService.RootFolder.RegisterTaskDefinition(taskName, taskDefinition);
