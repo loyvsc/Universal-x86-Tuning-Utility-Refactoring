6100565dc6cd887c488e266b2de229e641c2fb77
Valery Varaksa <varaksav62@gmail.com>
2026-03-21 14:23:51 +0300

    Introduce localization tools

---
+++ Universal x86 Tuning Utility/Localization/Converters/StringLocalizationConverter.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Universal_x86_Tuning_Utility.Localization.Converters;
+
+/// <summary>
+/// Converter to use strings in XAML. Support localization.
+/// </summary>
+public class StringLocalizationConverter : IValueConverter
+{
+    /// <inheritdoc/>
+    object IValueConverter.Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string str)
+        {
+            throw new NotSupportedException("Value should be enum.");
+        }   
+
+        return ProgramCore.Localizer[str];
+    }
+
+    /// <inheritdoc/>
+    object IValueConverter.ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return string.Empty;
+    }
+}
---
+++ Universal x86 Tuning Utility/Localization/Models/Language.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationCore.Utilities;
+
+namespace Universal_x86_Tuning_Utility.Localization.Models;
+
+public class Language : NotifyPropertyChangedBase
+{
+    private string _name;
+    private string _shortName = string.Empty;
+    private string _key;
+
+    public static readonly Language UnknownLang = new Language(string.Empty, string.Empty);
+
+    public string Name
+    {
+        get => _name;
+        set => SetValue(ref _name, value);
+    }
+
+    public string ShortName
+    {
+        get => _shortName;
+        set => SetValue(ref _shortName, value);
+    }
+
+    public string Key
+    {
+        get => _key;
+        set => SetValue(ref _key, value);
+    }
+
+    public Language(string key, string name)
+    {
+        _key = key;
+        var keyValues = key.Split('-');
+        if (keyValues.Length == 2)
+        {
+            _shortName = keyValues[1];
+        }
+        _name = name;
+    }
+}
